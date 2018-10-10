095022b2d855db834ac8b00f11976c9aed0bed62
David Stockhouse <stockhod@my.erau.edu>
2018-10-09 20:43:27 -0700

    Added to MDE example main

--- mde/main.c
+++ mde/main.c
@@ -23,9 +23,55 @@
  * Test for the MDE software.
  */
 int main() {
+// Initialize UART to MDE board
+        mdeDevice->uart_fd = mde_init();
+        if (mdeDevice->uart_fd) {
+                // Initialization failed
+#ifdef  ES_DEBUG_MODE
+                printf("Couldn't initialize MDE UART.\n");
+                return mdeDevice->uart_fd;
+#endif          
+        }
+ 	
+ 	     
+		// Check for MDE input
+		while(serialDataAvail(mdeDevice->uart_fd)) {
+			// Read serial input
+			rc = es_uartGetChar(mdeDevice->uart_fd);
+			if(rc) {
+#ifdef	ES_DEBUG_MODE
+				printf("Failed to read from MDE channel\n");
+				return rc;
+#endif
+			}
+		}
+             
+		rc = mde_parseData(&mdeDevice);
+		if(rc) {
+#ifdef	ES_DEBUG_MODE
+			printf("Failed\n");
+			return rc;
+#endif
+		}
+              
+ 	     return 0;
+              
+		/***** Services to execute every 15 minutes *****/
+		if (cycleCounter % (15 * 60) == 0 && cycleCount > 0) {
 
+			// Get MDE health data
+			mde_requestHealthPacket(mdeDevice->uart_fd);
 
+		} // 15 minute cycle counter
+
+		// Increment cycle counter for task scheduling
+		cycleCount++;
+
+	} // while(!abortTest)
+
+	// End a successful test
 	return 0;
 
-} // Function main()
-
+ } // Function main()
+              
+            
